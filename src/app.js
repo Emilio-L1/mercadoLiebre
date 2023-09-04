8b566a5153ad6505e7f32965c251e548808099fb
Emilio-L1 <lomaazul.80@gmail.com>
2023-09-03 22:58:03 -0300

    img/cuerpo/estructura

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ app.listen(3080, () => console.log("Servidor escuchando en el puerto 3080!"))
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/index.html"));
+app.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
 app.get("/contacto", (req, res) => {
